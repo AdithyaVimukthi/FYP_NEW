ed51e4f3e7b1754acabf3d1579e326a9b897848f
AdithyaVimukthi <adithyavimukthi23@gmail.com>
2023-10-06 07:18:19 +0530

    pi_side_new_code

--- files_from_pi/main.py
+++ files_from_pi/main.py
@@ -1,7 +1,9 @@
 from Server_gui import ServerMonitorApp
 from PyQt5.QtWidgets import *
 import sys
+import os
 
+os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
