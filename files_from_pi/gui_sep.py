f3b0f02544af0603b15f0f346e7d0cc8d3d60645
AdithyaVimukthi <adithyavimukthi23@gmail.com>
2023-10-06 11:41:36 +0530

    with video

--- files_from_pi/gui_sep.py
+++ files_from_pi/gui_sep.py
@@ -1,34 +1,21 @@
 import sys
-import cv2
 import socket
 import threading
-import pickle
-import struct
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from servo import servo_controller
 from camera import camera
-
-
-def get_ip_address():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        ip_address = s.getsockname()[0]
-        s.close()
-        return ip_address
-    except Exception as e:
-        return "Error: " + str(e)
-
+from server_sep import Server
+from cus_threading import ThreadWithReturnValue
 
 class ServerMonitorApp(QMainWindow):
     def __init__(self):
         super().__init__()
+        
         self.servo = servo_controller()
         self.cam = camera()
 
-        self.frame = None
         self.clients_label = None
         self.save_list = None
         self.server_thread = None
@@ -36,16 +23,13 @@ class ServerMonitorApp(QMainWindow):
         self.clear_list = None
         self.start_button = None
         self.client_listbox = None
-        self.server_socket = None
         self.ClientCount = 0
 
-        self.server_port = 8000
         self.client_messages = []
         self.server_running = False
-        self.disconnect_msg = 'disconnect'
 
         self.init_ui()
-
+    
     def init_ui(self):
         self.setWindowTitle("Robot Controller")
         self.setGeometry(50, 50, 1150, 600)
@@ -61,7 +45,7 @@ class ServerMonitorApp(QMainWindow):
         data_main = QVBoxLayout()
         ipdata_and_cam.addLayout(data_main)
 
-        ip_address = get_ip_address()
+        ip_address = "get_ip_address()"
         ip_label = QLabel(f"Your IP Address:          {ip_address}")
         ip_label.setFont(QFont('Arial', 30))
         data_main.addWidget(ip_label)
@@ -172,18 +156,18 @@ class ServerMonitorApp(QMainWindow):
         self.showFullScreen()
 
     def update_webcam_feed(self):
-        self.frame = self.cam.capture()
-        self.frame  = cv2.flip(self.frame , 0)
+        frame = self.cam.capture()
         
         # Convert the frame to a QImage
-        height, width, channel = self.frame.shape
+        height, width, channel = frame.shape
         bytes_per_line = 3 * width
-        q_img = QImage(self.frame.data, width, height, bytes_per_line, QImage.Format_RGB888)
+        q_img = QImage(frame.data, width, height, bytes_per_line, QImage.Format_RGB888)
 
         # Display the webcam feed in the QLabel
         self.cam_label.setPixmap(QPixmap.fromImage(q_img))
     
     def start_server(self):
+        self.server = Server()
         self.server_running = True
         self.server_thread = threading.Thread(target=self.run_server)
         self.server_thread.daemon = True
@@ -225,91 +209,3 @@ class ServerMonitorApp(QMainWindow):
                 QMessageBox.information(self, "Success", "List data saved successfully!")
             except Exception as e:
                 QMessageBox.critical(self, "Error", f"An error occurred while saving: {str(e)}")
-
-    def run_server(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((get_ip_address(), self.server_port))
-        self.server_socket.listen(5)
-        self.client_listbox.clear()
-
-        self.start_button.setEnabled(False)
-
-        Running_msg = "Server Running ..............~ "
-        self.client_listbox.addItem(Running_msg)
-        self.client_messages.append(Running_msg)
-
-        while True:
-            if self.server_running:
-                try:
-                    client_socket, client_address = self.server_socket.accept()
-                    client_info = f"Client connected: {client_address[0]}:{client_address[1]}"
-                    self.ClientCount += 1
-                    self.clients_label.setText(f"Connected Clients:         {self.ClientCount}")
-                    self.client_listbox.addItem(client_info)
-                    self.client_messages.append(client_info)
-                    self.servo.to_init_pos()
-
-                    self.clear_list.setEnabled(False)
-                    self.save_list.setEnabled(False)
-
-                    # Handle client disconnect
-                    client_thread = threading.Thread(target=self.handle_client, args=(client_socket, client_address))
-                    client_thread.daemon = True
-                    client_thread.start()
-                except Exception as e:
-                    print("Error accepting client:", str(e))
-
-    def handle_client(self, client_socket, client_address):
-        while True:
-            try:
-                self.video_send(client_socket)
-                data = client_socket.recv(1024)
-                if not data:
-                    break
-                data_dec = data.decode('utf-8')
-                if data_dec == self.disconnect_msg:
-                    break
-                
-                self.received_messages_list.addItem(f"{data_dec}")
-                self.received_messages_list.scrollToBottom()
-
-                mot_ang_msg = self.servo.controll(data_dec)
-
-                self.mr_angle_list.addItem(mot_ang_msg[1])
-                self.mr_angle_list.scrollToBottom()
-                self.ml_angle_list.addItem(mot_ang_msg[2])
-                self.ml_angle_list.scrollToBottom()
-                self.gripper_state_list.addItem(mot_ang_msg[0])
-                self.gripper_state_list.scrollToBottom()
-
-                # Process received data here if needed
-            except Exception as e:
-                print("Error receiving data from client:", str(e))
-                break
-
-        # Handle client disconnect
-        client_socket.close()
-        disconnected_msg = f"Client disconnected: {client_address[0]}:{client_address[1]}"
-        self.ClientCount -= 1
-        self.clients_label.setText(f"Connected Clients:         {self.ClientCount}")
-        self.client_listbox.addItem(disconnected_msg)
-        self.client_messages.append(disconnected_msg)
-        self.servo.to_init_pos()
-
-        self.clear_list.setEnabled(True)
-        self.save_list.setEnabled(True)
-
-        list_count = self.received_messages_list.count()
-        print(f"list_count = {list_count - 1}")
-
-    def video_send(self, conn):
-        data = pickle.dumps(self.frame)
-        message_size = struct.pack("Q", len(data))
-        conn.sendall(message_size + data)
-
-
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     server_monitor = ServerMonitorApp()
-#     server_monitor.show()
-#     sys.exit(app.exec_())
